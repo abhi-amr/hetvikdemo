8d0b2aadc4e672bd4de0cd684c6c3a58b514c60d
rupeshstar <narayanrupesh43@gmail.com>
2021-03-17 18:47:54 +0530

    Rupesh,17/03/2021

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,30 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Navbar/Header";
 import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
 import Home from "./pages/Home/Home";
 import AboutUs from "./pages/About/AboutUs";
 import ContactUs from "./pages/Contact/ContactUs";
 import GoalsMission from "./components/AboutUs/GoalsMission";
 import Team from "./components/AboutUs/Team";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PageNotFound from "./components/Errors/PageNotFound";
-import QFilter from './components/QPaper/QFilter';
+import QFilter from "./components/QPaper/QFilter";
 import Questions from "./pages/Questions/Questions";
-
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header />
-          <Switch>
-            <Route path = "/" exact component = {Home} />
-            <Route path = "/about" exact component = {AboutUs} />
-            <Route path = "/contact" exact component = {ContactUs} />
-            <Route path = "/team" exact component = {Team} />
-            <Route path = "/questions" exact component = {Questions} />
-            
-            
-          </Switch>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={AboutUs} />
+          <Route path="/contact" exact component={ContactUs} />
+          <Route path="/team" exact component={Team} />
+          <Route path="/questions" exact component={Questions} />
+          <Route path="/login" exact component={Login} />
+        </Switch>
         <Footer />
       </Router>
     </div>
--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -1,35 +1,54 @@
-import React, {Component} from 'react';
-import {Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import {Link} from "react-router-dom";
-import './Navbar.css';
+import React, { Component } from "react";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
-class Header extends Component{
+class Header extends Component {
   render() {
     return (
-        <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-              <Navbar.Brand><Link className="navbar-brand" to="/"><strong>hetvik</strong></Link></Navbar.Brand>
-              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link ><Link className="nav-link" to="/questions">Questions</Link></Nav.Link>
-                  <Nav.Link ><Link className="nav-link" to="/contact">Contact</Link></Nav.Link>
-                  <Nav.Link ><Link className="nav-link" to="/about">About</Link></Nav.Link>
-                  {/* <NavDropdown title="Years" id="collasible-nav-dropdown">
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand>
+            <Link className="navbar-brand" to="/">
+              <strong>hetvik</strong>
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link>
+                <Link className="nav-link" to="/questions">
+                  Questions
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link className="nav-link" to="/contact">
+                  Contact
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </Nav.Link>
+              {/* <NavDropdown title="Years" id="collasible-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">2020</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.2">2019</NavDropdown.Item>
                     <NavDropdown.Item href="#action/3.3">2018</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">Course</NavDropdown.Item>
                   </NavDropdown> */}
-                </Nav>
-                <Nav>
-                  <Nav.Link className="sign">Sign In</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-            
-        </div>
+            </Nav>
+            <Nav>
+              <Nav.Link className="sign">
+                <Link className="nav-link" to="/login">
+                  Sign In
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
