96e88268f2bf5818dbb030c6ce98a5841b683586
abhi-amr <abhi-amr@users.noreply.github.com>
2021-04-19 22:10:16 +0530

    qfetched updated to <object/>

--- src/components/QPaper/QFetched.jsx
+++ src/components/QPaper/QFetched.jsx
@@ -19,11 +19,18 @@ class QFetched extends Component {
     //const customizedPdf = `${this.props.link}` + '#toolbar=0';
     return (
       <div>
-        <embed
+        {/* <embed
           src={this.props.pdflink}
           type="application/pdf"
           height="610px"
           width="100%"
+        /> */}
+
+        <object 
+          data={this.props.pdflink}
+          type="application/pdf"
+          width="100%"
+          height="610px"
         />
       </div>
     );
