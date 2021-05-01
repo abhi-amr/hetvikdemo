fc7bccbe31b6b874602a51ab4e706eb57bb4e2bd
abhi-amr <abhi-amr@users.noreply.github.com>
2021-05-01 22:04:59 +0530

    avatar for team testing

--- src/components/AboutUs/CoreTeam.jsx
+++ src/components/AboutUs/CoreTeam.jsx
@@ -2,9 +2,12 @@ import React, {Component} from 'react';
 import {CardGroup, CardDeck, Card, Container, Row, Col} from 'react-bootstrap';
 import Font, { Text } from 'react-font';
 import {  } from 'react-icons/fa';
-import anand from '../../images/Avatar/avatar2.jpg';
-import narayan from '../../images/Avatar/avatar3.jpg';
-import kumar from '../../images/Avatar/avatar1.jpg';
+// import anand from '../../images/Avatar/avatar2.jpg';
+// import narayan from '../../images/Avatar/avatar3.jpg';
+// import kumar from '../../images/Avatar/avatar1.jpg';
+import anand from '../../images/Avatar/abhishekAd.svg';
+import narayan from '../../images/Avatar/rupesh.svg';
+import kumar from '../../images/Avatar/abhishekKr.svg';
 
 
 
